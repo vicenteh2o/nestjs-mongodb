b768ece8f287ff73fb317808b6f5bb2bee15c070
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-05-26 11:42:06 -0300

    feat: validate id on getusersbyid

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body, Get, Param } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param, HttpException } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/CreateUser.dto";
+import mongoose from "mongoose";
 
 @Controller('users')
 export class UsersController {
@@ -17,7 +18,11 @@ export class UsersController {
     }
 
     @Get(':id')
-    getUserById(@Param('id') id: string) {
-        return this.usersService.getUserById(id);
+    async getUserById(@Param('id') id: string) {
+        const isValid = mongoose.Types.ObjectId.isValid(id);
+        if (!isValid) throw new HttpException('User not found', 404);
+        const findUser = await this.usersService.getUserById(id);
+        if (!findUser) throw new HttpException('User not found', 404);
+        return findUser;
     }
 }
