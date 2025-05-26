47c0aa45878c839fa1af9fdea06bc08020a00b0a
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-05-26 13:49:13 -0300

    feat: update user and validations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Get, Param, HttpException } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param, HttpException, Patch } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/CreateUser.dto";
 import mongoose from "mongoose";
+import { UpdateUserDto } from "./dto/UpdateUser.dto";
 
 @Controller('users')
 export class UsersController {
@@ -24,5 +25,14 @@ export class UsersController {
         const findUser = await this.usersService.getUserById(id);
         if (!findUser) throw new HttpException('User not found', 404);
         return findUser;
+    }
+
+    @Patch(':id')
+    async updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        const isValid = mongoose.Types.ObjectId.isValid(id);
+        if (!isValid) throw new HttpException('Invalid ID', 400);
+        const updatedUser = await this.usersService.updateUser(id, updateUserDto);
+        if (!updatedUser) throw new HttpException('User not found', 404);
+        return updatedUser;
     }
 }
