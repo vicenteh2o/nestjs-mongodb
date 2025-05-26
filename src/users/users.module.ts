dc40332648c1052135fb9a04288e2de74f358f89
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-05-26 15:04:28 -0300

    feat: usersettings entity and one to one relationship plus populate settings key on user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,20 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "src/schemas/User.schema";
 import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
+import { UserSettings, UserSettingsSchema } from "src/schemas/UserSettings.schema";
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{
+        MongooseModule.forFeature([
+        {
             name: User.name,
             schema: UserSchema
-        }])
+        },
+        {
+            name: UserSettings.name,
+            schema: UserSettingsSchema
+        }
+    ]),
     ],
     providers: [UsersService],
     controllers: [UsersController]
